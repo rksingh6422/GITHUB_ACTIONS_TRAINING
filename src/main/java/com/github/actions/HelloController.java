e5cc34da2fb8014e1af30a185b05c20e8df8ff51
rksingh6422 <rk.singh6422@gmail.com>
2025-08-19 13:50:59 +0530

    Updated code
    Updated code

--- src/main/java/com/github/actions/HelloController.java
+++ src/main/java/com/github/actions/HelloController.java
@@ -3,6 +3,7 @@ package com.github.actions;
 import com.github.actions.entity.Greeting;
 import com.github.actions.repository.GreetingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +20,15 @@ public class HelloController {
         return "Welcome!!! Github Actions Training with Docker and Kubernetes";
     }
 
-    @GetMapping("/fetchGreeting")
-    public String showHome(Model model) {
-        Greeting dockerGreeting = repository.findById(1).orElse(new Greeting("Not Found 😕"));
-        model = model.addAttribute("name", dockerGreeting.getName());
-        return "home";
+    @GetMapping("/fetchGreeting/{id}")
+    public ResponseEntity<Greeting> showHome(Integer id) {
+
+        Greeting dockerGreeting = repository.findById(id).orElse(new Greeting("Not Found 😕"));
+        if (dockerGreeting != null) {
+            return ResponseEntity.ok(dockerGreeting); // Returns 200 OK with user object
+        } else {
+            return ResponseEntity.notFound().build(); // Returns 404 Not Found
+        }
     }
 
 }
