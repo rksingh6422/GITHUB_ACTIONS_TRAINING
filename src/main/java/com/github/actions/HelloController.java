59bda1eb6ad01e7a361debfc00c312d0f67f7951
rksingh6422 <rk.singh6422@gmail.com>
2025-08-19 15:51:39 +0530

    updated path

--- src/main/java/com/github/actions/HelloController.java
+++ src/main/java/com/github/actions/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class HelloController {
     }
 
     @GetMapping("/fetchGreeting/{id}")
-    public ResponseEntity<Greeting> showHome(Integer id) {
+    public ResponseEntity<Greeting> showHome(@PathVariable Integer id) {
 
         Greeting dockerGreeting = repository.findById(id).orElse(new Greeting("Not Found 😕"));
         if (dockerGreeting != null) {
